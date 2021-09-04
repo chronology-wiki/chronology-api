b7e17980b81e3f06f109679f83a77954788635d6
Joel Denning <joeldenning@gmail.com>
2021-09-04 16:17:12 -0600

    Revert "Cors"
    This reverts commit 62ef4f990d69e7f31007b69c762bde720975b753.

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,6 @@ use diesel::pg::PgConnection;
 use diesel::Connection;
 use dotenv::dotenv;
 use std::env;
-use rocket_cors::{AllowedHeaders, AllowedOrigins};
 
 /* Declaring a module, just for separating things better */
 pub mod topics;
@@ -40,25 +39,11 @@ pub fn establish_connection() -> PgConnection {
 }
 
 fn main() {
-    let allowed_origins = AllowedOrigins::All;
-    let cors_result = rocket_cors::CorsOptions {
-        allowed_origins,
-        allowed_methods: vec![rocket::http::Method::Get].into_iter().map(From::from).collect(),
-        allowed_headers: AllowedHeaders::default(),
-        allow_credentials: true,
-        ..Default::default()
-    }
-    .to_cors();
-
-    let cors = cors_result.unwrap();
-
     rocket::ignite().mount("/", routes![
         topics::list, 
         topics::get_topic,
         events::list,
         perspectives::list,
         perspectives::perspective_events_endpoint
-        ])
-        .attach(cors)
-        .launch();
+        ]).launch();
 }
